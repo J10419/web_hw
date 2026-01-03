90f6b02e47a02a6ec8293415a27e3afc0e4bb7ab
john <sean8861014@gmail.com>
2026-01-03 16:44:57 +0800

    fix navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,38 +1,61 @@
 function renderNavbar() {
+    const navbarContainer = document.querySelector('#navbar-area');
 
-  const navbarContainer = document.querySelector('#navbar');
+    if (!navbarContainer) return;
 
-  if (navbarContainer) {
+    const currentPath = window.location.pathname;
+
+    let rootPath = './';
+    let htmlPath = 'html/';
+
+    if (currentPath.includes('/html/')) {
+        rootPath = '../';
+        htmlPath = './';
+    }
+
+    const logoUrl = "img/logo.jpg";
+
+    const schoolUrl = "https://www.pu.edu.tw/";
+
     navbarContainer.innerHTML = `
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">web</a>
+
+          <a class="navbar-brand" href="${rootPath}index.html">
+            <img src="${logoUrl}" alt="Logo" width="30" height="30" class="d-inline-block align-text-top rounded-circle">web
+          </a>
 
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
           </button>
 
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
               <li class="nav-item">
-                <a class="nav-link" href="./index.html">首頁</a>
+                <a class="nav-link" href="${rootPath}index.html">首頁</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./schedule.html">課表查詢</a>
-              </li>
-               <li class="nav-item">
-                <a class="nav-link" href="./rps-game.html">猜拳遊戲</a>
+                <a class="nav-link" href="${htmlPath}schedule.html">課表</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./quiz.html">猜謎挑戰</a>
+                <a class="nav-link" href="${htmlPath}rps.html">猜拳</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="${htmlPath}quiz.html">猜謎</a>
               </li>
             </ul>
+
+            <div class="d-flex">
+                <a href="${schoolUrl}" target="_blank" class="btn btn-outline-light btn-sm">
+                    靜宜大學
+                </a>
+            </div>
+
           </div>
         </div>
       </nav>
-
-      `;
-  }
+    `;
 }
 
 renderNavbar();
